2b8931642654cbd5701bfca856bb4246cd18e362
gioblasco <giovanna.blasco@hotmail.com>
2018-03-24 19:05:42 -0300

    corpo de muitas funções feito, conferir

--- Compilador/Compiler.java
+++ Compilador/Compiler.java
@@ -17,123 +17,238 @@ public class Compiler {
     //   program ::= PROGRAM id BEGIN pgm_body END
     public void program(){
         if(lexer.token != Symbol.PROGRAM)
-            error.signal("Error! Missing PROGRAM keyword on the line: " + lexer.getLineNumber());
+            error.signal("Error: Missing PROGRAM keyword at line: " + lexer.getLineNumber());
 
         lexer.nextToken();
 
         if(lexer.token!= Symbol.IDENT)
-			error.signal("Error! Missing PROGRAM Identifier on the line: " + lexer.getLineNumber());
+					error.signal("Error: Missing PROGRAM Identifier at line: " + lexer.getLineNumber());
 
-		lexer.nextToken();
+				lexer.nextToken();
 
-		if(lexer.token != Symbol.BEGIN)
-            error.signal("Error! Missing BEGIN keyword on the line: " + lexer.getLineNumber());
+				if(lexer.token != Symbol.BEGIN)
+            error.signal("Error: Missing BEGIN keyword at line: " + lexer.getLineNumber());
 
-		lexer.nextToken();
+				lexer.nextToken();
 
-		pgm_body();
+				pgm_body();
 
         if(lexer.token!= Symbol.END)
-            error.signal("Missing BEGIN keyword");
+            error.signal("Error: Missing END keyword at line:" + lexer.getLineNumber());
+
         lexer.nextToken();
     }
 
 	// pgm_body -> decl func_declarations
 	public void pgm_body(){
+		decl();
 
+		func_declarations();
+
+		lexer.nextToken();
 	}
 	// decl -> string_decl_list {decl} | var_decl_list {decl} | empty
 	public void decl(){
+		if(lexer.token == Symbol.STRING){
+			string_decl_list();
 
+			decl();
+		}
+		else if(lexer.token == Symbol.FLOAT || lexer.token == Symbol.INT){
+			var_decl_list();
+
+			decl();
+		}
 	}
 
 	/* Global String Declaration */
 	// string_decl_list -> string_decl {string_decl_tail}
 	public void string_decl_list(){
+		string_decl();
 
+		string_decl_tail();
 	}
 
 	// string_decl -> STRING id := str ; | empty
 	public void string_decl(){
+			if(lexer.token == Symbol.STRING){
+				lexer.nextToken();
 
+				if(lexer.token != Symbol.IDENT)
+					error.signal("Error: Missing identifier at line: " + lexer.getLineNumber());
+				lexer.nextToken();
+
+				if(lexer.token != Symbol.ASSIGN)
+					error.signal("Error: Missing assignment symbol at line: " + lexer.getLineNumber());
+				lexer.nextToken();
+
+				if(lexer.token != Symbol.STRING)
+					error.signal("Error: Missing STRINGLITERAL type at line: " + lexer.getLineNumber());
+				lexer.nextToken();
+
+				if(lexer.token != Symbol.SEMICOLON)
+					error.signal("Error: Missing end of declaration at line: " + lexer.getLineNumber());
+				lexer.nextToken();
+
+			}
 	}
 
 	// string_decl_tail -> string_decl {string_decl_tail}
 	public void string_decl_tail(){
+		string_decl();
 
+		string_decl_tail();
 	}
 
 	/* Variable Declaration */
 	// var_decl_list -> var_decl {var_decl_tail}
 	public void var_decl_list(){
+		var_decl();
 
+		var_decl_tail();
 	}
 
 	// var_decl -> var_type id_list ; | empty
 	public void var_decl(){
-
+		if(lexer.token == Symbol.FLOAT || lexer.token == Symbol.INT){
+			var_type();
+			id_list();
+			if(lexer.token == Symbol.SEMICOLON)
+				error.signal("Error: Missing end of declaration at line: " + lexer.getLineNumber());
+			lexer.nextToken();
+		}
 	}
 
 	// var_type -> FLOAT | INT
 	public void var_type(){
-
+		if(lexer.token != Symbol.FLOAT && lexer.token == Symbol.INT)
+			error.signal("Error: Wrong variable type at line: " + lexer.getLineNumber());
+		lexer.nextToken();
 	}
 
 	// any_type -> var_type | VOID
 	public void any_type(){
-
+		if(lexer.token == Symbol.FLOAT || lexer.token == Symbol.INT)
+			var_type();
+		else if(lexer.token != Symbol.VOID)
+			error.signal("Error: Wrong type at line: " + lexer.getLineNumber());
+		lexer.nextToken();
 	}
 
 	// id_list -> id id_tail
 	public void id_list(){
-
+		if(lexer.token == Symbol.IDENT){
+			lexer.nextToken();
+			id_tail();
+		} else
+				error.signal("Error: Wrong id_list declaration at line: " + lexer.getLineNumber());
+		lexer.nextToken();
 	}
 
 	// id_tail -> , id id_tail | empty
 	public void id_tail(){
+		if(lexer.token == Symbol.COMMA){
+			lexer.nextToken();
 
+			if(lexer.token != Symbol.IDENT)
+				error.signal("Error: Missing identifier at line: " + lexer.getLineNumber());
+			lexer.nextToken();
+
+			id_tail();
+		}
 	}
 
 	// var_decl_tail -> var_decl {var_decl_tail}
 	public void var_decl_tail(){
+		var_decl();
 
+		var_decl_tail();
 	}
 
 	/* Function Paramater List */
 	// param_decl_list -> param_decl param_decl_tail
 	public void param_decl_list(){
+		param_decl();
 
+		param_decl_tail();
 	}
 
 	// param_decl -> var_type id
 	public void param_decl(){
+		if(lexer.token == Symbol.FLOAT || lexer.token == Symbol.INT){
+			var_type();
 
+			if(lexer.token != Symbol.IDENT)
+				error.signal("Error: Missing identifier at line: " + lexer.getLineNumber());
+			lexer.nextToken();
+		}
 	}
 
 	// param_decl_tail -> , param_decl param_decl_tail | empty
 	public void param_decl_tail(){
+		if(lexer.token == Symbol.COMMA){
+				lexer.nextToken();
 
+				param_decl();
+
+				param_decl_tail();
+		}
 	}
 
 	/* Function Declarations */
 	// func_declarations -> func_decl {func_decl_tail}
 	public void func_declarations(){
+		func_decl();
 
+		func_decl_tail();
 	}
 
 	// func_decl -> FUNCTION any_type id ({param_decl_list}) BEGIN func_body END | empty
 	public void func_decl(){
+		if(lexer.token == Symbol.FUNCTION){
+			lexer.nextToken();
 
+			any_type();
+
+			if(lexer.token != Symbol.IDENT)
+				error.signal("Error: Missing identifier at line: " + lexer.getLineNumber());
+			lexer.nextToken();
+
+			if(lexer.token != Symbol.LPAR)
+				error.signal("Error: Missing parantheses at line: " + lexer.getLineNumber());
+			lexer.nextToken();
+
+			//NÃO SEI FAZER WHILE PRO param_decl_list (talvez uma condição de parada seja == ")", mas se n tiver ")" entra em loop infinito)
+			param_decl_list();
+
+			if(lexer.token != Symbol.RPAR)
+				error.signal("Error: Missing parantheses at line: " + lexer.getLineNumber());
+			lexer.nextToken();
+
+			if(lexer.token != Symbol.BEGIN)
+				error.signal("Error: Missing BEGIN keyword at line: " + lexer.getLineNumber());
+			lexer.nextToken();
+
+			func_body();
+
+			if(lexer.token != Symbol.END)
+				error.signal("Error: Missing END keyword at line: " + lexer.getLineNumber());
+			lexer.nextToken();
+		}
 	}
 
 	// func_decl_tail -> func_decl {func_decl_tail}
 	public void func_decl_tail(){
+		func_decl();
 
+		func_decl_tail();
 	}
 
 	// func_body -> decl stmt_list
 	public void func_body(){
+		decl();
 
+		stmt_list();
 	}
 
 	/* Statement List */
@@ -230,48 +345,118 @@ public class Compiler {
 
  	// addop -> + | -
 	public void addop(){
-
+		if(lexer.token != Symbol.PLUS && lexer.token != Symbol.MINUS)
+			error.signal("Error: Wrong operator at line: " + lexer.getLineNumber());
+		lexer.nextToken();
 	}
 
 	// mulop -> * | /
 	public void mulop(){
-
+		if(lexer.token != Symbol.MULT && lexer.token != Symbol.DIV)
+			error.signal("Error: Wrong operator at line: " + lexer.getLineNumber());
+		lexer.nextToken();
 	}
 
 	/* Complex Statements and Condition */
 	// if_stmt -> IF ( cond ) THEN stmt_list else_part ENDIF
 	public void if_stmt(){
+		if(lexer.token == Symbol.IF){
+			lexer.nextToken();
 
+			if(lexer.token != Symbol.LPAR)
+				error.signal("Error: Missing parantheses at line: " + lexer.getLineNumber());
+			lexer.nextToken();
+
+			cond();
+
+			if(lexer.token != Symbol.RPAR)
+				error.signal("Error: Missing parantheses at line: " + lexer.getLineNumber());
+			lexer.nextToken();
+
+			if(lexer.token != Symbol.THEN)
+				error.signal("Error: Missing THEN keyword at line: " + lexer.getLineNumber());
+			lexer.nextToken();
+
+			stmt_list();
+
+			else_part();
+
+			if(lexer.token != Symbol.ENDIF)
+				error.signal("Error: Missing ENDIF keyword at line: " + lexer.getLineNumber());
+			lexer.nextToken();
+
+		} else
+				error.signal("Error: Missing IF keyword at line: " + lexer.getLineNumber());
 	}
 
 	// else_part -> ELSE stmt_list | empty
 	public void else_part(){
+		if(lexer.token == Symbol.ELSE){
+			lexer.nextToken();
 
+			stmt_list();
+		}
 	}
 
 	// cond -> expr compop expr
 	public void cond(){
+		expr();
 
+		compop();
+
+		expr();
 	}
 
 	// compop -> < | > | =
 	public void compop(){
-
+		if(lexer.token != Symbol.LT && lexer.token != Symbol.GT && lexer.token != Symbol.EQUAL)
+			error.signal("Error: Missing comparison operator at line: " + lexer.getLineNumber());
+		lexer.nextToken();
 	}
 
 	// for_stmt -> FOR ({assign_expr}; {cond}; {assign_expr}) stmt_list ENDFOR
 	public void for_stmt(){
+		if(lexer.token == Symbol.FOR){
+			lexer.nextToken();
 
+			if(lexer.token != Symbol.LPAR)
+				error.signal("Error: Missing parantheses at line: " + lexer.getLineNumber());
+			lexer.nextToken();
+
+			assign_expr();
+
+			if(lexer.token != Symbol.SEMICOLON)
+				error.signal("Error: Missing end of declaration at line: " + lexer.getLineNumber());
+			lexer.nextToken();
+
+			cond();
+
+			if(lexer.token != Symbol.SEMICOLON)
+				error.signal("Error: Missing end of declaration at line: " + lexer.getLineNumber());
+			lexer.nextToken();
+
+			assign_expr();
+
+			if(lexer.token != Symbol.SEMICOLON)
+				error.signal("Error: Missing end of declaration at line: " + lexer.getLineNumber());
+			lexer.nextToken();
+
+			if(lexer.token != Symbol.RPAR)
+				error.signal("Error: Missing parantheses at line: " + lexer.getLineNumber());
+			lexer.nextToken();
+
+			stmt_list();
+
+			if(lexer.token != Symbol.ENDFOR)
+				error.signal("Error: Missing ENDFOR keyword at line: " + lexer.getLineNumber());
+			lexer.nextToken();
+			
+		} else
+				error.signal("Error: Missing FOR keyword at line: " + lexer.getLineNumber());
 	}
 
-   /* public boolean Oper()
-    {
-        if(lexer.token == Symbol.PLUS || lexer.token == Symbol.MINUS || lexer.token == Symbol.DIV || lexer.token == Symbol.MULT)
-            return true;
-        return false;
-    }
-    */
+
 	private Lexer lexer;
-    private CompilerError error;
+  private CompilerError error;
 
 }
