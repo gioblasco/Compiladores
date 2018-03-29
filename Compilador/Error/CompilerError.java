c3c9c8518d6a6dad3555affb3236b31d99c5790b
gioblasco <giovanna.blasco@hotmail.com>
2018-03-29 15:09:04 -0300

    correção de alguns erros

--- Compilador/Error/CompilerError.java
+++ Compilador/Error/CompilerError.java
@@ -4,17 +4,17 @@ import Lexer.*;
 import java.io.*;
 
 public class CompilerError {
-    
+
     public CompilerError( Lexer lexer ) {
           // output of an error is done in out
         this.lexer = lexer;
         thereWasAnError = false;
     }
-    
+
     public void setLexer( Lexer lexer ) {
         this.lexer = lexer;
     }
-    
+
     public boolean wasAnErrorSignalled() {
         return thereWasAnError;
     }
@@ -22,30 +22,29 @@ public class CompilerError {
     public void show( String strMessage ) {
         show( strMessage, false );
     }
-    
+
     public void show( String strMessage, boolean goPreviousToken ) {
         // is goPreviousToken is true, the error is signalled at the line of the
         // previous token, not the last one.
         if ( goPreviousToken ) {
-          System.out.println("Error at line " + lexer.getCurrentLine() + ": ");
+          System.out.println("Error not expected (" + lexer.token.toString() + ") at line " + lexer.getCurrentLine() + ": ");
         }
         else {
-          System.out.println("Error at line " + lexer.getLineNumber() + ": ");
+          System.out.println("Error not expected (" + lexer.token.toString() + ") at line " + lexer.getLineNumber() +": ");
           System.out.println(lexer.getCurrentLine());
         }
-        
+
         System.out.println( strMessage );
-        
+
         thereWasAnError = true;
     }
-       
+
     public void signal( String strMessage ) {
         show( strMessage );
         thereWasAnError = true;
         throw new RuntimeException();
     }
-    
+
     private Lexer lexer;
     private boolean thereWasAnError;
 }
-    
