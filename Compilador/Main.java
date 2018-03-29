ddf7053932d632e1e949b29bdf3d375c038084f5
gioblasco <giovanna.blasco@hotmail.com>
2018-03-29 14:23:46 -0300

    reparação

--- Compilador/Main.java
+++ Compilador/Main.java
@@ -2,12 +2,12 @@ import java.io.*;
 
 public class Main {
     public static void main( String []args ) {
-        
+
         File file;
         FileReader stream;
         int numChRead;
-        
-        if ( args.length != 1 ) 
+
+        if ( args.length != 1 )
             System.out.println("Use only one parameter, the file to be compiled");
         else {
            file = new File(args[0]);
@@ -15,8 +15,8 @@ public class Main {
              System.out.println("Either the file " + args[0] + " does not exist or it cannot be read");
              throw new RuntimeException();
            }
-           try { 
-             stream = new FileReader(file);  
+           try {
+             stream = new FileReader(file);
             } catch ( FileNotFoundException e ) {
                 System.out.println("Something wrong: file does not exist anymore");
                 throw new RuntimeException();
@@ -24,30 +24,29 @@ public class Main {
                 // one more character for '\0' at the end that will be added by the
                 // compiler
             char []input = new char[ (int ) file.length() + 1 ];
-            
+
             try {
               numChRead = stream.read( input, 0, (int ) file.length() );
             } catch ( IOException e ) {
                 System.out.println("Error reading file " + args[0]);
                 throw new RuntimeException();
             }
-                
-            if ( numChRead != file.length() ) {
-                System.out.println("Read error");
-                throw new RuntimeException();
-            }
+
+            // if ( numChRead != file.length() ) {
+            //     System.out.println("Read error");
+            //     throw new RuntimeException();
+            // }
             try {
               stream.close();
             } catch ( IOException e ) {
                 System.out.println("Error in handling the file " + args[0]);
                 throw new RuntimeException();
             }
-                
+
 
             Compiler compiler = new Compiler();
-        
+
             compiler.compile(input);
             }
     }
 }
-        
